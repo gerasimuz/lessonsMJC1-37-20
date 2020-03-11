ba80ce77cceb492ac6897cb7b1f11832f1aed889
Gerasimenko Vitaliy <gerasimuz@gmail.com>
2020-03-11 22:58:55 +0300

    первое сохранение 11.03

---
+++ lesson1/JC1-37-20/src/edu/academy/Main.java
@@ -0,0 +1,41 @@
+package edu.academy;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("byte min " + Byte.MIN_VALUE + " max " + Byte.MAX_VALUE);
+        byte b = 127; // -128...127
+        Byte bR = Byte.MIN_VALUE; //-128...127
+
+        long l = 0;
+        Long lR = 0L;
+
+        float f = 8.0f;
+        Float fR = .1F;
+
+        double d = .0;
+        Double dR = .0D;
+
+        //0l 0L
+        //0f 0F .1F
+        //0d 0D .1D
+
+         int a;
+        Integer c;
+
+//        a = a + 1;
+//
+//        System.out.println(a);
+//        System.out.println(c);
+
+//        short
+//        char
+//        int
+//        long
+//        float
+//        double
+//        boolean
+    }
+}
---
+++ lesson1/JC1-37-20/src/edu/academy/Man.java
@@ -0,0 +1,9 @@
+package edu.academy;
+
+public class Man {
+    int hairLength;
+
+    public void grow(){
+        hairLength = hairLength + 1;
+    }
+}
