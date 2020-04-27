37d4c11b40fc871cbd1d50b15d6b355057e0889b
Gerasimenko Vitaliy <gerasimuz@gmail.com>
2020-04-27 17:50:44 +0300

    DZ8

--- -=Domasnie zadaniya/DZ8/src/edu/academy/_1MainWriteToFile.java
+++ -=Domasnie zadaniya/DZ8/src/edu/academy/_1MainWriteToFile.java
@@ -8,7 +8,7 @@ public class _1MainWriteToFile {
     //создаем 100000 обьектов типа студент со случайным именем и записываем в файл.!!!!!!!!!
     public static void main(String[] args) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src//edu//academy//students.bin"))){
-            for (int i = 0; i < 100000 ; i++) {
+            for (int i = 0; i < 100 ; i++) {
                 Student s = new Student();
                 oos.writeObject(s);
 
