581336e6ee3728567cc002db48c1dadc60f1b7cc
Gerasimenko Vitaliy <gerasimuz@gmail.com>
2020-05-16 18:26:56 +0300

    1

---
+++ -=Domasnie zadaniya/DZ9/src/edu/academy/Main.java
@@ -0,0 +1,15 @@
+package edu.academy;
+
+public class Main {
+
+    public static void main(String [] args)    {
+        String textMy = "";
+        textMy = new String (Files.readAllBytes(Paths.get(academy/Benign_list.csv)));
+
+        
+    }
+
+    }
+
+}
+
